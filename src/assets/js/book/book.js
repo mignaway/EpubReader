e61fcb5a01756ee09a2544102d0abb3a793a527c
mignaway <40722616+mignaway@users.noreply.github.com>
2022-06-17 20:18:40 +0200

    book pagination (not completed)

--- src/assets/js/book/book.js
+++ src/assets/js/book/book.js
@@ -7,9 +7,14 @@ var loadBookHtml = async function() {
     console.log(__dirname)
     var response = await EPub.createAsync(__dirname + "/epubs/" + epubCodeSearch + "/epub.epub", 'epubs/' + epubCodeSearch, 'epubs/' + epubCodeSearch)
         .then(async function (epub) {
+            console.log(epub.metadata)
+            epub.getFile("css", async function (error, data, mimeType) { 
+                await fse.outputFile(__dirname + "/epubs/" + epubCodeSearch + "/css.css", data, 'binary')
+                $('head').append('<link rel="stylesheet" type="text/css" href="epubs/' + epubCodeSearch + '/css.css">');
+            })
             await epub.flow.forEach(function (chapter) {
                 epub.getChapter(chapter.id, function (error, text) {
-                    $('#book-content-columns').append(text)
+                    $('#book-content-columns').append(`<div id="${chapter.id}" class="chapter-content">${text}</div>`);
                 });
             })
         })
