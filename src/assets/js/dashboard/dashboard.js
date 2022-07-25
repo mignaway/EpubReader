47742e305e02b5101b99d9082452b7b4e15808f6
mignaway <40722616+mignaway@users.noreply.github.com>
2022-07-26 00:46:34 +0200

    start reading text

--- src/assets/js/dashboard/dashboard.js
+++ src/assets/js/dashboard/dashboard.js
@@ -24,14 +24,16 @@ var loadAll = async function (books_json){
 async function loadHeroSection(books_json, sortby) {
     if (books_json.length > 0) {
         
-        const [title, author, bookYear, language, folderBookCode, coverPath] = [books_json[0].title, 
+        const [title, author, bookYear, language, folderBookCode, coverPath, bookOpened] = [books_json[0].title, 
                                                                                 books_json[0].author ? books_json[0].author : 'Undefined Author', 
                                                                                 books_json[0].bookYear ? books_json[0].bookYear : 'Undefined', 
                                                                                 books_json[0].lang ? books_json[0].lang : 'Undefined Language', 
                                                                                 books_json[0].folderBookCode, 
-                                                                                books_json[0].coverPath]
+                                                                                books_json[0].coverPath,
+                                                                                books_json[0].lastPageOpened != null]
 
         const dominantRGBValue = await getVibrantColorFromImage(__dirname + '/epubs/' + folderBookCode + '/' + coverPath)
+        var keepReadingText = bookOpened ? 'Keep reading' : 'Start reading';
         await $('#hero-section-content')
             .html(`
             <div id="hero-section-image-cover"><img src="${'epubs/' + folderBookCode + '/' + coverPath}"></div>
@@ -39,7 +41,7 @@ async function loadHeroSection(books_json, sortby) {
               <h1 class="main-text text-color-white text-b">${title}</h1>
               <h2 class="main-text text-color-white">${author}</h2>
               <h3 class="main-text text-color-white op-5">${bookYear} Edition - ${language}</h3>
-              <a href="book.html?code=${folderBookCode}" id="keep-reading-button" onmouseover="this.style.backgroundColor='rgba(${dominantRGBValue},0.5)'" onmouseout="this.style.backgroundColor='rgb(${dominantRGBValue})'" class="primary-button" style="background-color: rgb(${dominantRGBValue})">Keep reading</a>
+              <a href="book.html?code=${folderBookCode}" id="keep-reading-button" onmouseover="this.style.backgroundColor='rgba(${dominantRGBValue},0.5)'" onmouseout="this.style.backgroundColor='rgb(${dominantRGBValue})'" class="primary-button" style="background-color: rgb(${dominantRGBValue})">${keepReadingText}</a>
             </div>
         `)
         $('#hero-section-image-background').css('background-image', 'linear-gradient(180deg, rgba(0, 0, 0, 0.8) 0%, rgba(0, 0, 0, 0.8) 100%), url(epubs/' + folderBookCode + '/' + coverPath);
