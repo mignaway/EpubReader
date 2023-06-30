f329770d81ca0c936dc123c8dea6a41a8996441f
mignaway <40722616+mignaway@users.noreply.github.com>
2023-06-30 14:57:00 +0200

    index update comments

--- src/index.js
+++ src/index.js
@@ -3,106 +3,109 @@ const path = require('path');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
-  // eslint-disable-line global-require
-  app.quit();
+	// eslint-disable-line global-require
+	app.quit();
 }
 
 const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 1280,
-    height: 720,
-    resizable: false,
-    show: false,
-    frame: false,
-    icon: path.join(__dirname,"icon.ico"),
-    // hasShadow: true,
-    // transparent: true,
-    webPreferences: {
-      webviewTag: true,
-      nodeIntegration: true,
-      contextIsolation: true,
-      enableRemoteModule: false,
-      preload: path.join(__dirname, './preload.js'),
-    },
-  });
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'pages', 'index.html'));
+	// Create the browser window.
+	const mainWindow = new BrowserWindow({
+		width: 1280,
+		height: 720,
+		resizable: false,
+		show: false,
+		frame: false,
+		icon: path.join(__dirname, "icon.ico"),
+		webPreferences: {
+			webviewTag: true,
+			nodeIntegration: true,
+			contextIsolation: true,
+			enableRemoteModule: false,
+			preload: path.join(__dirname, './preload.js'),
+		},
+	});
 
-  ipcMain.handle('appVersion', () => app.getVersion())
-  ipcMain.handle('storePath', () => path.resolve(path.join(app.getPath('userData'),'localStorage')).split(path.sep).join("/"))
+	// Load the index.html of the app.
+	mainWindow.loadFile(path.join(__dirname, 'pages', 'index.html'));
 
-  mainWindow.webContents.setWindowOpenHandler((details) => {
-    shell.openExternal(details.url);
-    return { action: 'deny' }
-  })
+	// IPC handlers
+	ipcMain.handle('appVersion', () => app.getVersion());
+	ipcMain.handle('storePath', () => path.resolve(path.join(app.getPath('userData'), 'localStorage')).split(path.sep).join("/"));
 
-  // mainWindow.on('ready-to-show', ()=> setTimeout(() => mainWindow.show(), 50))
-  mainWindow.webContents.on('did-finish-load', async () => {
-    setTimeout(() => mainWindow.show(), 50); // HACK
-  });
-	globalShortcut.register('f5', ()=> {
-    mainWindow.reload()
-  })
+	// Open external URLs in default browser
+	mainWindow.webContents.setWindowOpenHandler((details) => {
+		shell.openExternal(details.url);
+		return { action: 'deny' };
+	});
 
-  ipcMain.on('closeApp', () => {
-    mainWindow.close()
-  })
-  ipcMain.on('minimizeApp', () => {
-    mainWindow.minimize()
-  })
-  ipcMain.on('resizeApp', () => {
-    if (!mainWindow.isMaximized()) {
-      mainWindow.maximize();
-      return null;
-    }
-    mainWindow.unmaximize();
-  })
-  ipcMain.on('unmaximizeApp', () => {
-    if (mainWindow.isMaximized()) {
-      mainWindow.unmaximize();
-      return null;
-    } 
-  })
-  ipcMain.on('openBookChooserDialog', () => {
-    dialog.showOpenDialog({ 
-      properties: ['openFile'],
-      filters: [
-        { "name": "Epub Files","extensions": ["epub"]}
-      ]
-    }).then(result => {
-      if (!result.canceled) {
-        mainWindow.webContents.send('bookChosenSuccess', result.filePaths[0])
-      }
-    }).catch(err => {
-      console.log(err)
-    })
-  })
+	// Show the window when the content finishes loading
+	mainWindow.webContents.on('did-finish-load', () => {
+		setTimeout(() => mainWindow.show(), 50); // Delayed show to avoid visual glitches
+	});
+
+	// Register global shortcut for refreshing the window
+	globalShortcut.register('f5', () => {
+		mainWindow.reload();
+	});
+
+	// Event listeners for window actions
+	ipcMain.on('closeApp', () => {
+		mainWindow.close();
+	});
+	ipcMain.on('minimizeApp', () => {
+		mainWindow.minimize();
+	});
+	ipcMain.on('resizeApp', () => {
+		if (!mainWindow.isMaximized()) {
+			mainWindow.maximize();
+		} else {
+			mainWindow.unmaximize();
+		}
+	});
+	ipcMain.on('unmaximizeApp', () => {
+		if (mainWindow.isMaximized()) {
+			mainWindow.unmaximize();
+		}
+	});
+
+	// Show a file dialog to choose an EPUB file
+	ipcMain.on('openBookChooserDialog', () => {
+		dialog
+			.showOpenDialog({
+				properties: ['openFile'],
+				filters: [
+					{ name: 'Epub Files', extensions: ['epub'] }
+				]
+			})
+			.then(result => {
+				if (!result.canceled) {
+					// Send the selected file path to the renderer process
+					mainWindow.webContents.send('bookChosenSuccess', result.filePaths[0]);
+				}
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	});
+
 	mainWindow.on('resize', () => {
-    mainWindow.webContents.send('updateMaximizeIcon', mainWindow.isMaximized());
-  })
-
+		mainWindow.webContents.send('updateMaximizeIcon', mainWindow.isMaximized());
+	});
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+// Event listener for when Electron has finished initialization
 app.on('ready', createWindow);
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+// Quit when all windows are closed, except on macOS
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+	// Re-create the window when the dock icon is clicked (only for macOS)
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createWindow();
+	}
 });
-
